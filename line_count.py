cd240cac0c9ce267cdc655e0c235bfcf8f26ce1c
Zederbear <hooverzed@gmail.com>
2024-09-04 14:24:58 -0600

    Did the assignment

--- line_count.py
+++ line_count.py
@@ -1,4 +1,4 @@
 def line_count():
-  """
-  Reference the "Reading Files in Python" lesson to open the `file.txt` file, count the number of words in the file, and return the count.
-  """
+    file = open('file.txt', 'r')
+    lines = file.readlines()
+    return len(lines)
