f95fc296d7bc468f44ef2429781f112a81265e0c
Zederbear <hooverzed@gmail.com>
2024-09-05 12:34:31 -0600

    added comments and closed the txt file

--- line_count.py
+++ line_count.py
@@ -1,4 +1,12 @@
 def line_count():
+    # opens file
     file = open('file.txt', 'r')
+
+    # reads the lines of the file
     lines = file.readlines()
+
+    # closes the file
+    file.close()
+
+    # returns the length of the list we got from reading the lines
     return len(lines)
